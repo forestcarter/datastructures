6f5f0f10ab40154104c5c86cba679f6283794940
forest carter <forest.carter@gmail.com>
2019-07-21 12:33:06 -0400

    added fetch and loading effect

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
 import LinkedList from "./LinkedList";
 class App extends Component {
     constructor() {
--- src/components/LinkedList.tsx
+++ src/components/LinkedList.tsx
@@ -5,48 +5,73 @@ import { Button } from "react-bootstrap";
 interface IProps {}
 
 interface Nodes {
-    nodeId: number;
+	nodeId: number;
+	filmName:string
 }
+
 interface IState {
-    nodes: Nodes[];
+	nodes: Nodes[];
+	loading:boolean
 }
 
 class LinkedList extends Component<IProps, IState> {
-    state: IState = { nodes: [{ nodeId: 0 }, { nodeId: 1 }, { nodeId: 2 }] };
+    state: IState = { nodes: [], loading:true};
 
-    addElement = (e: any) => {
-        this.setState(prevState => ({
-            nodes: [
-                ...prevState.nodes,
-                {
-                    nodeId: prevState.nodes.length
-                }
-            ]
-        }));
-	};
+    addElement  = async (e: any) => {
+		this.setState(() => ({ loading:true }));
+		await this.getFilmName(this.state.nodes.length);
+		this.setState(() => ({ loading:false }));
+	}
 
 	subtractElement = (e: any) => {
         this.setState(prevState => ({
             nodes: 
                 prevState.nodes.slice(0,-1),
         }));
-    };
+	};
+
+	getFilmName = async (indexNum:number)=>{
+			const rawResponse = await fetch(`https://swapi.co/api/films/${indexNum+1}/?format=json`);
+			let dataResult = await rawResponse.json();
+			this.setState(prevState => ({
+				nodes: [
+					...prevState.nodes,
+					{
+						nodeId: indexNum,
+						filmName: dataResult.title,
+					}
+				]
+			}));
+			return dataResult.title;
+	}
+	
+	async componentDidMount(){
+		this.setState(() => ({ loading:true }));
+		await this.getFilmName(0);
+		await this.getFilmName(1);
+		await this.getFilmName(2);
+		this.setState(() => ({ loading:false }));
+
+
+		//0 [0,1,2].map(await this.getFilmName).
+		//const filmNames = await this.getFilmName.call(null,...[0,1,2])	
+	}
 
     render() {
         return (
             <div>
                 <h2 className="sectionHeader">Linked List</h2>
                 <div className="flexRow">
-				<Button className="addButton" onClick={this.subtractElement}>
+				<Button disabled={this.state.loading} className="addButton" onClick={this.subtractElement}>
                         Delete Item
                     </Button>
                 
                     <div className="linkedListContainer">
                         {this.state.nodes.map(node => {
-                            return <div key={node.nodeId}>{node.nodeId}</div>;
+                            return <div key={node.nodeId}>{node.filmName}</div>;
                         })}
                     </div>
-                    <Button className="addButton" onClick={this.addElement}>
+                    <Button disabled={this.state.loading || this.state.nodes.length>=7} className="addButton" onClick={this.addElement}>
                         Add Item
                     </Button>
                 </div>

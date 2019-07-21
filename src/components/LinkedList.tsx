61031dddcc4e459679d38b9634fce63736cfd18e
forest carter <forest.carter@gmail.com>
2019-07-21 19:21:16 -0400

    Implemented Linked List

--- src/components/LinkedList.tsx
+++ src/components/LinkedList.tsx
@@ -2,50 +2,105 @@ import React, { Component } from "react";
 import "./LinkedList.css";
 import { Button } from "react-bootstrap";
 
-interface IProps {}
-
-interface Nodes {
+class Node {
     nodeId: number;
     filmName: string;
+    next: any;
+    constructor(nodeId: number, filmName: string) {
+        this.nodeId = nodeId;
+        this.filmName = filmName;
+        this.next = null;
+    }
 }
 
+class singlyLinkList {
+    head: any;
+    tail: any;
+    length: number;
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(nodeId: number, filmName: string) {
+        var newNode = new Node(nodeId, filmName);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    pop() {
+        if (this.head === null) return undefined;
+        var current = this.head;
+        var newTail = current;
+        while (current.next) {
+            newTail = current;
+            current = current.next;
+        }
+        this.length -= 1;
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = newTail;
+            this.tail.next = null;
+        }
+        return current;
+    }
+}
+
+interface IProps {}
+
 interface IState {
-    nodes: Nodes[];
+    linkedList: singlyLinkList;
     loading: boolean;
 }
 
 class LinkedList extends Component<IProps, IState> {
-    state: IState = { nodes: [], loading: true };
+    state: IState = { linkedList: new singlyLinkList(), loading: true };
 
     addElement = async (e: any) => {
         this.setState(() => ({ loading: true }));
-        await this.getFilmName(this.state.nodes.length);
+        await this.getFilmName(this.state.linkedList.length);
         this.setState(() => ({ loading: false }));
+    };
+
+    returnAfterPop = (listToPop: any) => {
+        const newList: singlyLinkList = listToPop;
+        newList.pop();
+        return newList;
     };
 
     subtractElement = (e: any) => {
         this.setState(prevState => ({
-            nodes: prevState.nodes.slice(0, -1)
+            linkedList: this.returnAfterPop(prevState.linkedList)
         }));
     };
 
     getFilmName = async (indexNum: number) => {
-		const query = ` query { allFilms { title } } `;
-        const rawResponse = await fetch("https://api.graphcms.com/simple/v1/swapi", {
-			method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({query})
-        });
-		let dataResult = await rawResponse.json();
+        const query = ` query { allFilms { title } } `;
+        const rawResponse = await fetch(
+            "https://api.graphcms.com/simple/v1/swapi",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ query })
+            }
+        );
+        let dataResult = await rawResponse.json();
 
         this.setState(prevState => ({
-            nodes: [
-                ...prevState.nodes,
-                {
-                    nodeId: indexNum,
-                    filmName: dataResult.data.allFilms[indexNum].title
-                }
-            ]
+            linkedList: prevState.linkedList.push(
+                indexNum,
+                dataResult.data.allFilms[indexNum].title
+            )
         }));
         return dataResult.title;
     };
@@ -56,12 +111,15 @@ class LinkedList extends Component<IProps, IState> {
         await this.getFilmName(1);
         await this.getFilmName(2);
         this.setState(() => ({ loading: false }));
-
-        //0 [0,1,2].map(await this.getFilmName).
-        //const filmNames = await this.getFilmName.call(null,...[0,1,2])
     }
 
     render() {
+        let currentHead = this.state.linkedList.head;
+        const arrayToRender = [];
+        while (currentHead) {
+            arrayToRender.push(currentHead);
+            currentHead = currentHead.next;
+        }
         return (
             <div>
                 <h2 className="sectionHeader">Linked List</h2>
@@ -75,13 +133,15 @@ class LinkedList extends Component<IProps, IState> {
                     </Button>
 
                     <div className="linkedListContainer">
-                        {this.state.nodes.map(node => {
+                        {arrayToRender.map(node => {
                             return <div key={node.nodeId}>{node.filmName}</div>;
                         })}
                     </div>
+					
                     <Button
                         disabled={
-                            this.state.loading || this.state.nodes.length >= 7
+                            this.state.loading ||
+                            this.state.linkedList.length >= 7
                         }
                         className="addButton"
                         onClick={this.addElement}

dfc389d6979beab01a9657c4ed67071b49a85fdd
forest <forest.carter@gmail.com>
2019-07-28 18:52:58 -0400

    ok

---
+++ src/setupTests.js
@@ -0,0 +1,9 @@
+import requestAnimationFrame from './tempPolyfills';
+ 
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+global.fetch = require('jest-fetch-mock')
+
+ 
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
